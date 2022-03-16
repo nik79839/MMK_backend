3b81d84ebb790e046514dcded149297ffd05d38c
nik79 <otrok2@mail.ru>
2022-03-16 21:59:43 +0700

    Добавьте файлы проекта.

---
+++ ForRastr/RastrFunc.cs
@@ -0,0 +1,141 @@
+﻿using ASTRALib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForRastr
+{
+    public class RastrFunc
+    {
+        /// <summary>
+        /// Возвращает индекс узла по номеру
+        /// </summary>
+        /// <param name="rastr">Объект растра</param>
+        /// <param name="ny">Номер узла</param>
+        /// <returns></returns>
+        public static int FindNodeIndex(Rastr rastr, int ny)
+        {
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol number = (ICol)node.Cols.Item("ny");
+            for (int index = 0; index < node.Count; index++)
+            {
+                if (Convert.ToDouble(number.ZN[index]) == ny)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Утяжеление по заданной траектории
+        /// </summary>
+        /// <param name="rastr">объект растра</param>
+        /// <param name="ut2Path">Путь к файлу утяжеления в формате ut2</param>
+        public static void Worsening(Rastr rastr, string ut2Path) // Осуществляет процедуру утяжеления.
+        {
+            // Первая строчка: Параметры/Утяжеление/Формировать описание... (вроде в траектории хранится)
+            rastr.Load(RG_KOD.RG_REPL, ut2Path, ut2Path);
+            if (rastr.ut_Param[ParamUt.UT_FORM_P] == 0)
+            {
+                rastr.ClearControl();
+                RastrRetCode kod = rastr.step_ut("i");
+                if (kod == 0)
+                {
+                    RastrRetCode kd;
+                    do
+                    {
+                        kd = rastr.step_ut("z");
+                        if (((kd == 0) && (rastr.ut_Param[ParamUt.UT_ADD_P] == 0)) || rastr.ut_Param[ParamUt.UT_TIP] == 1)
+                        {
+                            rastr.AddControl(-1, "");
+                        }
+                    }
+                    while (kd == 0);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Случайное утяжеление, утяжеляет нагрузки на случайный процент до расхождения режима
+        /// </summary>
+        /// <param name="rastr"></param>
+        /// <param name="nodes">Узлы утяжеления</param>
+        /// <param name="tgvalues">Список cos f, генерируется в другом методе случайным образом</param>
+        /// <param name="unode">Список напряжений </param>
+        /// <param name="percent">Диапазон в процентах от 0</param>
+        public static void WorseningRandom(Rastr rastr, List<int> nodes, List<double> tgvalues, List<List<double>> unode, int percent)
+        {
+            List<double> U = new List<double>();
+            Random randPercent = new Random();
+            rastr.rgm("p");
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol pn = (ICol)node.Cols.Item("pn");
+            ICol qn = (ICol)node.Cols.Item("qn");
+            ICol Ur = (ICol)node.Cols.Item("vras");
+            RastrRetCode kod = rastr.rgm("p");
+            int index;
+            if (kod == 0)
+            {
+                RastrRetCode kd;
+                do
+                {
+                    kd = rastr.rgm("p");
+                    for (int i = 0; i < nodes.Count; i++)
+                    {
+                        index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                        pn.set_ZN(index, Convert.ToDouble(pn.Z[index]) * (1 + (float)randPercent.Next(0, percent) / 100));
+                        qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgvalues[i]);
+                    }
+                    kd = rastr.rgm("p");
+                }
+                while (kd == 0);
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                    U.Add(Convert.ToDouble(Ur.Z[index]));
+                }
+                unode.Add(U);
+            }
+        }
+
+        /// <summary>
+        /// Случайная нагрузка для каждой реализации
+        /// </summary>
+        /// <param name="rastr"></param>
+        /// <param name="nodes">Список узлов с нагрузкой</param>
+        /// <param name="tgValues">Списко cos а для узлов нагрузки</param>
+        public static void ChangePn(Rastr rastr, List<int> nodes, List<double> tgValues)
+        {
+            Random randPn = new Random();
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol pn = (ICol)node.Cols.Item("pn");
+            ICol qn = (ICol)node.Cols.Item("qn");
+            RastrRetCode kod = rastr.rgm("p");
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                int index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                pn.set_ZN(index, (float)randPn.Next(Convert.ToInt32(pn.ZN[index]) * 10 / 2, Convert.ToInt32(pn.ZN[index]) * 15) / 10f);
+                qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgValues[i]);
+            }
+        }
+
+        /// <summary>
+        /// Случайный tg для каждой реалиации
+        /// </summary>
+        /// <param name="rastr"></param>
+        /// <param name="nodes">Узлы</param>
+        /// <param name="tgValue">Список значений cos f</param>
+        public static void ChangeTg(Rastr rastr, List<int> nodes, List<double> tgValue)
+        {
+            tgValue.Clear();
+            Random randtg = new Random();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                tgValue.Add((float)randtg.Next(48, 62) / 100);
+            }
+        }
+    }
+}
---
+++ Диплом_УР_Автоматизация/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using ASTRALib;
+using Excel = Microsoft.Office.Interop.Excel;
+using OfficeOpenXml;
+using ForRastr;
+
+namespace Диплом_УР_Автоматизация
+{
+    public static class RastrTemplate
+    {
+        public const string rg2 = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер2.rg2";
+        public const string ut2 = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер.ut2";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rastr rastr = new Rastr();
+            Random rand = new Random();
+            string rg2Path = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер2.rg2";
+            string ut2Path = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер.ut2";
+            rastr.Load(RG_KOD.RG_REPL, rg2Path, RastrTemplate.rg2);
+            Console.WriteLine("Режим загружен.");
+            rastr.Load(RG_KOD.RG_REPL, ut2Path, RastrTemplate.ut2);
+            Console.WriteLine("Траектория утяжеления загружена.");
+            List<int> NagrNodes = new List<int>() //Список узлов нагрузки
+            {
+                1654, 2654, 2652,
+                2649, 2647,2646
+            };
+            List<int> NagrValue = new List<int>() //Список максимумов нагрузки
+            {
+                80, 92, 112,
+                139, 259,1411
+            };
+            List<double> tgNodes = new List<double>(); //Список коэф мощности для каждой реализации
+            List<List<double>> Unodes = new List<List<double>>(); // Список напряжений для каждого узла каждой реализации
+            List<double> powerFlow = new List<double>(); // Список значений Pпред
+            List<List<double>> data = new List<List<double>>(); //Список реализаций факторов
+            List<List<int>> factors = new List<List<int>>(); //Список списков значений факторов (всех)
+            int exp = 100; // Число реализаций
+            for (int i = 0; i < exp; i++)
+            {
+                rastr.Load(RG_KOD.RG_REPL, rg2Path, RastrTemplate.rg2);
+                ASTRALib.ITable node = (ITable)rastr.Tables.Item("node");
+                ICol P = (ICol)node.Cols.Item("pg");
+                ICol Pn = (ICol)node.Cols.Item("pn");
+                ICol Qn = (ICol)node.Cols.Item("qn");
+                RastrFunc.ChangeTg(rastr, NagrNodes, tgNodes);
+                ChangePn(rastr, NagrNodes, tgNodes);
+                rastr.rgm("p");
+                WorseningRandom(rastr, NagrNodes, tgNodes,Unodes,30);
+                Console.WriteLine(Math.Round(Convert.ToDouble(P.Z[7]), 2));
+                powerFlow.Add(Convert.ToDouble(P.Z[7]));
+            }
+            //ToExcel(powerFlow,Unodes, 4);
+        }
+        public static void WorseningRandom(Rastr rastr, List<int> nodes, List<double> tgvalues, List<List<double>> unode,int ex) // Осуществляет процедуру утяжеления, увеличивая нагрузку по процентам
+        {
+            List<double> U = new List<double>();
+            Random randPercent = new Random();
+            rastr.rgm("p");
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol pn = (ICol)node.Cols.Item("pn");
+            ICol qn = (ICol)node.Cols.Item("qn");
+            ICol Ur = (ICol)node.Cols.Item("vras");
+            RastrRetCode kod = rastr.rgm("p");
+            int index;
+                if (kod == 0)
+                {
+                    RastrRetCode kd;
+                    do
+                    {
+                        kd = rastr.rgm("p");
+                    for (int i = 0; i < nodes.Count; i++)
+                    {
+                        index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                        pn.set_ZN(index, Convert.ToDouble(pn.Z[index])* (1+(float)randPercent.Next(0,30)/100));
+                        qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgvalues[i]);
+                    }
+                    kd = rastr.rgm("p");
+                }
+                    while (kd == 0);
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                    U.Add(Convert.ToDouble(Ur.Z[index]));
+                }
+                unode.Add(U);
+                }
+        }
+        public static void ToExcel(List<double> powerFlow, List<List<double>> U, int list) // Запись в Excel мощностей и напряжений
+        {
+            string path = @"C:\Users\otrok\Pictures\Книга1.xlsm";
+            using (var excel = new ExcelPackage(new System.IO.FileInfo(path)))
+            {
+                var ws = excel.Workbook.Worksheets[list];
+                for (int k = 0; k < powerFlow.Count; k++)
+                {
+                    ws.Cells[k + 2, 1].Value = powerFlow[k];
+                }
+                for (int i=0;i < U.Count; i++) // Реализации
+                {
+                    for (int k=0; k < U[i].Count; k++) // Узлы
+                    {
+                        ws.Cells[i+2, k+19].Value = U[i][k];
+                    }
+                }
+                excel.Save();
+            }
+        }
+
+        public static void ChangePn(Rastr rastr, List<int> nodes, List<double> tgValues) //Случайная нагрузка для каждой реализации
+        {
+            Random randPn = new Random();
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol pn  = (ICol)node.Cols.Item("pn");
+            ICol qn = (ICol)node.Cols.Item("qn");
+            RastrRetCode kod = rastr.rgm("p");
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    int index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
+                    pn.set_ZN(index, (float)randPn.Next(Convert.ToInt32(pn.ZN[index]) * 10 / 2, Convert.ToInt32(pn.ZN[index]) * 15) / 10f);
+                    qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgValues[i]);
+                }
+        }
+    }
+}
