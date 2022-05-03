3eaa87b9026b7e2d5de9c63bb9f37cb623891d5f
nik79 <otrok2@mail.ru>
2022-05-03 12:17:53 +0700

    Добавление словарей для вывода в Excel с названиями колонок

--- Диплом_УР_Автоматизация/Program.cs
+++ Диплом_УР_Автоматизация/Program.cs
@@ -4,6 +4,7 @@ using ASTRALib;
 using Excel = Microsoft.Office.Interop.Excel;
 using OfficeOpenXml;
 using ForRastr;
+using System.Linq;
 
 namespace Диплом_УР_Автоматизация
 {
@@ -11,6 +12,7 @@ namespace Диплом_УР_Автоматизация
     {
         public const string rg2 = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер2.rg2";
         public const string ut2 = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\Экспер.ut2";
+        public const string sch = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\СБЭК_сечения.sch";
     }
     class Program
     {
@@ -20,10 +22,15 @@ namespace Диплом_УР_Автоматизация
             Random rand = new Random();
             string rg2Path = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\СБЭК_СХН.rg2";
             string ut2Path = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\СБЭК.ut2";
+            string schPath = @"C:\Users\otrok\Desktop\Дипломмаг\Мой\СБЭК_сечения.sch";
             rastr.Load(RG_KOD.RG_REPL, rg2Path, RastrTemplate.rg2);
             Console.WriteLine("Режим загружен.");
             rastr.Load(RG_KOD.RG_REPL, ut2Path, RastrTemplate.ut2);
             Console.WriteLine("Траектория утяжеления загружена.");
+            rastr.Load(RG_KOD.RG_REPL, schPath, RastrTemplate.sch);
+            Console.WriteLine("Сечения загружены.");
+            ITable sch = (ITable)rastr.Tables.Item("sechen");
+            ICol powerSech = (ICol)sch.Cols.Item("psech");
             List<int> NagrNodes = new List<int>() //Список узлов нагрузки
             {
                 1620, 1610, 2643,
@@ -31,7 +38,8 @@ namespace Диплом_УР_Автоматизация
                 60408124,60408125,60408126,60408122,60408121,60408120,60408119,2631,1617,641,2642,640,60405020,2653,1655,2647,2648,2649,
                 2639,60405028,1805,2645,2646,60405012,599,600,60405029,2603
             };
-            List<int> nodesForWorsening = RastrFunc.RayonNodesToList(rastr, 1); //Узлы района 1 (бодайб)
+            //List<int> nodesForWorsening = RastrFunc.RayonNodesToList(rastr, 1); //Узлы района 1 (бодайб)
+            List<int> nodesForWorsening = RastrFunc.RayonNodesToList(rastr, 1).Union(RastrFunc.RayonNodesToList(rastr, 2)).ToList();
             List<double> tgNodes = new List<double>(); //Список коэф мощности для каждой реализации
             List<List<double>> Unodes = new List<List<double>>(); // Список величин напряжений для каждого узла каждой реализации
             List<List<double>> Ibranches = new List<List<double>>(); // Список величин токов для каждого узла каждой реализации
@@ -40,23 +48,28 @@ namespace Диплом_УР_Автоматизация
             List<List<int>> factors = new List<List<int>>(); //Список списков значений факторов (всех)
             List<int> nodesWithKP = new List<int>() {2658,2643, 60408105 };
             List<int> nodesWithSkrm=new List<int>();
+            Dictionary<string, List<double>> UValueDict = new Dictionary<string, List<double>>(); //Словарь со значениями напряжений
+            Dictionary<string, List<double>> IValueDict = new Dictionary<string, List<double>>(); //Словарь со значениями токов
             RastrFunc.SkrmNodesToList(rastr, nodesWithSkrm); //Заполнение листа с узлами  СКРМ
-            List<int> brunchesWithAOPO = new List<int>() { RastrFunc.FindBranchIndex(rastr, 60408117, 70402816, 2), RastrFunc.FindBranchIndex(rastr, 2640, 2641, 0),
-                RastrFunc.FindBranchIndex(rastr, 2631, 2640, 0),RastrFunc.FindBranchIndex(rastr, 2639, 2640, 0),RastrFunc.FindBranchIndex(rastr, 2639, 60408105, 0),
-                RastrFunc.FindBranchIndex(rastr, 60408105, 2630, 1),RastrFunc.FindBranchIndex(rastr, 2620, 60408105, 0),RastrFunc.FindBranchIndex(rastr, 2639, 60408105, 0),
-                RastrFunc.FindBranchIndex(rastr, 2610, 60408105, 0),RastrFunc.FindBranchIndex(rastr, 2600, 60408105, 2)}; // Индексы
+            List<int> brunchesWithAOPO = new List<int>() { RastrFunc.FindBranchIndex(rastr, 2640, 2641, 0), RastrFunc.FindBranchIndex(rastr, 2631, 2640, 0),
+                RastrFunc.FindBranchIndex(rastr, 2639, 2640, 0),RastrFunc.FindBranchIndex(rastr, 2639, 60408105, 0), RastrFunc.FindBranchIndex(rastr, 60408105, 2630, 1),
+                RastrFunc.FindBranchIndex(rastr, 2620, 60408105, 0), RastrFunc.FindBranchIndex(rastr, 2610, 60408105, 0)}; // Индексы
             
+            ITable node = (ITable)rastr.Tables.Item("node");
+            ICol name = (ICol)node.Cols.Item("name");
+            ITable vetv = (ITable)rastr.Tables.Item("vetv");
+            ICol vetvName = (ICol)vetv.Cols.Item("name");
+            for (int i = 0; i < nodesWithKP.Count; i++) // Создание ключей в словаре
+            {
+                int index = RastrFunc.FindNodeIndex(rastr, nodesWithKP[i]);
+                UValueDict.Add(name.Z[index].ToString(), new List<double>());
+            }
+            foreach (var index in brunchesWithAOPO) IValueDict.Add(vetvName.Z[index].ToString(), new List<double>()); // Создание ключей в словаре
+
             int exp = 100; // Число реализаций
             for (int i = 0; i < exp; i++)
             {
                 rastr.Load(RG_KOD.RG_REPL, rg2Path, RastrTemplate.rg2);
-                ASTRALib.ITable node = (ITable)rastr.Tables.Item("node");
-                ASTRALib.ITable vetv = (ITable)rastr.Tables.Item("vetv");
-                ICol P = (ICol)node.Cols.Item("pg");
-                ICol Pn = (ICol)node.Cols.Item("pn");
-                ICol Qn = (ICol)node.Cols.Item("qn");
-                ICol name = (ICol)vetv.Cols.Item("name");
-                ICol powerStart = (ICol)vetv.Cols.Item("pl_ip");
                 //RastrFunc.ChangeNodeStateRandom(rastr, nodesWithSkrm); //вкл или выкл для СКРМ 50/50
                 RastrFunc.ChangeTg(rastr, NagrNodes, tgNodes);
                 ChangePn(rastr, NagrNodes, tgNodes);
@@ -64,47 +77,43 @@ namespace Диплом_УР_Автоматизация
                 if (test == RastrRetCode.AST_NB)
                 {
                     Console.WriteLine($"Итерация {i} не завершена из-за несходимости режима.");
-                    //rastr.Save($"разошелся {i}.rg2", "");
-                    // Вроде проблема решена
                     continue;
                 }
                 rastr.rgm("p");
                 //RastrFunc.Worsening(rastr, ut2Path);
-                double powerFlowValue = WorseningRandom(rastr, nodesForWorsening, tgNodes, Unodes, nodesWithKP,brunchesWithAOPO,Ibranches);
-                //double powerFlowSech = -Math.Round(Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 2658, 0)]) +
-                //    Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 1656, 0)]), 2);
+                double powerFlowValue = WorseningRandom(rastr, nodesForWorsening, tgNodes, nodesWithKP,brunchesWithAOPO,UValueDict,IValueDict);
+                //double powerFlowSech = Math.Round(Convert.ToDouble(powerSech.Z[1]),2);
                 Console.WriteLine(powerFlowValue + " "+i);
                 powerFlow.Add(powerFlowValue);
             }
-            ToExcel(powerFlow,Unodes, 7);
-            //ToExcel_I(Ibranches);
+            //ToExcel(powerFlow, 3,UValueDict);
+            ToExcel_I(IValueDict);
         }
-        public static double WorseningRandom(Rastr rastr, List<int> nodes, List<double> tgvalues, List<List<double>> unode, List<int> nodesWithKP,
-            List<int> brunchesWithAOPO, List<List<double>> IBranches) // Осуществляет процедуру утяжеления, увеличивая нагрузку по процентам
-        {
-            List<double> U = new List<double>();
-            List<double> I = new List<double>();
-            List<double> powerFlowStep = new List<double>();
+        public static double WorseningRandom(Rastr rastr, List<int> nodes, List<double> tgvalues, List<int> nodesWithKP,
+            List<int> brunchesWithAOPO, Dictionary<string,List<double>> UValueDict, Dictionary<string,List<double>> IValueDict) // Осуществляет процедуру утяжеления, увеличивая нагрузку по процентам
+        {         
             Random randPercent = new Random();
-            rastr.rgm("p");
             ITable node = (ITable)rastr.Tables.Item("node");
+            ITable sch = (ITable)rastr.Tables.Item("sechen");
+            ICol powerSech = (ICol)sch.Cols.Item("psech");
             ICol pn = (ICol)node.Cols.Item("pn");
             ICol qn = (ICol)node.Cols.Item("qn");
             ICol Ur = (ICol)node.Cols.Item("vras");
             ICol sta = (ICol)node.Cols.Item("sta");
-            ASTRALib.ITable vetv = (ITable)rastr.Tables.Item("vetv");
-            ICol powerStart = (ICol)vetv.Cols.Item("pl_ip");
+            ICol name = (ICol)node.Cols.Item("name");
+            ITable vetv = (ITable)rastr.Tables.Item("vetv");
+            ICol vetvName = (ICol)vetv.Cols.Item("name");
             ICol iMax = (ICol)vetv.Cols.Item("i_max");
             RastrRetCode kod = rastr.rgm("p");
-            float randomPercent = 0;
+            float randomPercent;
             int index;
+            
             if (kod == 0)
             {
                 RastrRetCode kd;
                 do
-                {
-                    
-                    for (int i = 0; i < nodes.Count; i++)
+                {                  
+                    for (int i = 0; i < nodes.Count; i++) //Основное утяжеление
                     {
                         index = RastrFunc.FindNodeIndex(rastr, nodes[i]);
                         randomPercent = 1 + (float)randPercent.Next(0, 15) / 100;
@@ -112,7 +121,7 @@ namespace Диплом_УР_Автоматизация
                         qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgvalues[i]);
                     }
                     kd = rastr.rgm("p");
-                    for (int i = 0; i < nodesWithKP.Count; i++) // Изменение СКРМ
+                    /*for (int i = 0; i < nodesWithKP.Count; i++) // Изменение СКРМ
                     {
                         index = RastrFunc.FindNodeIndex(rastr, nodesWithKP[i]);
                         if (Convert.ToDouble(Ur.Z[index]) < 225)
@@ -123,14 +132,9 @@ namespace Диплом_УР_Автоматизация
                             RastrFunc.ConnectedBranchState(rastr, 60408136, 2, 0);
                         }
                     }
-                    kd = rastr.rgm("p");
-                    powerFlowStep.Add(-Math.Round(Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 2658, 0)]) +
-                    Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 1656, 0)]), 2));
-                    //Console.WriteLine(-Math.Round(Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 2658, 0)]) +
-                    //Convert.ToDouble(powerStart.Z[RastrFunc.FindBranchIndex(rastr, 2654, 1656, 0)]), 2));
+                    kd = rastr.rgm("p");*/                   
                 }
                 while (kd == 0);
-                //int j = 1;
                 while (kd != 0) // Откат на последний сходяийся режим
                 {
                     for (int i = 0; i < nodes.Count; i++)
@@ -139,27 +143,23 @@ namespace Диплом_УР_Автоматизация
                         pn.set_ZN(index, Convert.ToDouble(pn.Z[index]) / 1.02);
                         qn.set_ZN(index, Convert.ToDouble(pn.ZN[index]) * tgvalues[i]);
                     }
-                    kd = rastr.rgm("p");
-                    
-                    //j++;
+                    kd = rastr.rgm("p");                  
                 }
                 for (int i = 0; i < nodesWithKP.Count; i++) // Запись напряжений
                 {
                     index = RastrFunc.FindNodeIndex(rastr, nodesWithKP[i]);
-                    U.Add(Convert.ToDouble(Ur.Z[index]));
+                    UValueDict[name.Z[index].ToString()].Add(Convert.ToDouble(Ur.Z[index]));
                 }
-                unode.Add(U);
                 for (int i = 0; i < brunchesWithAOPO.Count; i++) // Запись токов в ветвях с АОПО
                 {
-                    I.Add(Convert.ToDouble(iMax.Z[brunchesWithAOPO[i]]));
+                    IValueDict[vetvName.Z[brunchesWithAOPO[i]].ToString()].Add(Convert.ToDouble(iMax.Z[brunchesWithAOPO[i]]));
                 }
-                IBranches.Add(I);
             }
-            return powerFlowStep[powerFlowStep.Count - 2];
+            return Math.Round(Convert.ToDouble(powerSech.Z[1]), 2);
         }
-        public static void ToExcel(List<double> powerFlow, List<List<double>> U, int list) // Запись в Excel мощностей и напряжений
+        public static void ToExcel(List<double> powerFlow, int list, Dictionary<string, List<double>> UValueDict) // Запись в Excel мощностей и напряжений
         {
-            string path = @"C:\Users\otrok\Desktop\Дипломмаг\СБЭК.xlsx";
+            string path = @"C:\Users\otrok\Desktop\Дипломмаг\СБЭКv2.xlsx";
             using (var excel = new ExcelPackage(new System.IO.FileInfo(path)))
             {
                 var ws = excel.Workbook.Worksheets[list];
@@ -167,29 +167,38 @@ namespace Диплом_УР_Автоматизация
                 {
                     ws.Cells[k + 2, 1].Value = powerFlow[k];
                 }
-                for (int i=0;i < U.Count; i++) // Реализации
+                int column = 15; // Initialize for keys.
+                foreach (string key in UValueDict.Keys)
                 {
-                    for (int k=0; k < U[i].Count; k++) // Узлы
+                    int row = 1; // Initialize for values in key.
+                    ws.Cells[row, column].Value = key;
+                    foreach (var value in UValueDict[key])
                     {
-                        ws.Cells[i+2, k+19].Value = U[i][k];
+                        row++;
+                        ws.Cells[row, column].Value = value;
                     }
+                    column++; // increment for next key.
                 }
                 excel.Save();
             }
         }
 
-        public static void ToExcel_I(List<List<double>> I) // Запись в Excel мощностей и напряжений
+        public static void ToExcel_I(Dictionary<string, List<double>> IValueDict) // Запись в Excel мощностей и напряжений
         {
-            string path = @"C:\Users\otrok\Desktop\Дипломмаг\СБЭК.xlsx";
             using (var excel = new ExcelPackage())
             {
                 var ws = excel.Workbook.Worksheets.Add("Лист1");
-                for (int i = 0; i < I.Count; i++) // Реализации
+                int column = 1; // Initialize for keys.
+                foreach (string key in IValueDict.Keys)
                 {
-                    for (int k = 0; k < I[i].Count; k++) // Узлы
+                    int row = 1; // Initialize for values in key.
+                    ws.Cells[row, column].Value = key;
+                    foreach (var value in IValueDict[key])
                     {
-                        ws.Cells[i + 2, k + 1].Value = I[i][k];
+                        row++;
+                        ws.Cells[row, column].Value = Math.Round(value*1000,2);
                     }
+                    column++; // increment for next key.
                 }
                 excel.SaveAs(new System.IO.FileInfo(@"C:\Users\otrok\Desktop\Дипломмаг\Тест_I.xlsx")) ;
             }
